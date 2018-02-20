28413f9d2c996c4bd435ebf750ebdbfd7b54f4f3
Nishiduka <thomas.nishiduka@gmail.com>
2018-02-20 16:25:22 -0300

    first commit

---
+++ Backup/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Backup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtDataBase = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtDirectory = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mskPassword = new System.Windows.Forms.MaskedTextBox();
+            this.mskServer = new System.Windows.Forms.MaskedTextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbEscolha = new System.Windows.Forms.ComboBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(137, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 33);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Okay";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtDataBase);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.btnOpen);
+            this.groupBox1.Controls.Add(this.txtDirectory);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.mskPassword);
+            this.groupBox1.Controls.Add(this.mskServer);
+            this.groupBox1.Controls.Add(this.txtUser);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cmbEscolha);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(373, 343);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtDataBase
+            // 
+            this.txtDataBase.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.txtDataBase.Location = new System.Drawing.Point(125, 203);
+            this.txtDataBase.Name = "txtDataBase";
+            this.txtDataBase.Size = new System.Drawing.Size(153, 20);
+            this.txtDataBase.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "DataBase";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.btnOpen.Location = new System.Drawing.Point(284, 241);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 6;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtDirectory
+            // 
+            this.txtDirectory.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.txtDirectory.Location = new System.Drawing.Point(125, 241);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.ReadOnly = true;
+            this.txtDirectory.Size = new System.Drawing.Size(153, 20);
+            this.txtDirectory.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 244);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Select File";
+            // 
+            // mskPassword
+            // 
+            this.mskPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.mskPassword.Location = new System.Drawing.Point(125, 163);
+            this.mskPassword.Name = "mskPassword";
+            this.mskPassword.PasswordChar = '*';
+            this.mskPassword.Size = new System.Drawing.Size(153, 20);
+            this.mskPassword.TabIndex = 3;
+            // 
+            // mskServer
+            // 
+            this.mskServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.mskServer.Location = new System.Drawing.Point(125, 97);
+            this.mskServer.Mask = "000\\.000\\.000\\.000";
+            this.mskServer.Name = "mskServer";
+            this.mskServer.Size = new System.Drawing.Size(153, 20);
+            this.mskServer.TabIndex = 1;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.txtUser.Location = new System.Drawing.Point(125, 129);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(153, 20);
+            this.txtUser.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "User";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server";
+            // 
+            // cmbEscolha
+            // 
+            this.cmbEscolha.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbEscolha.FormattingEnabled = true;
+            this.cmbEscolha.Items.AddRange(new object[] {
+            "Backup",
+            "Restore"});
+            this.cmbEscolha.Location = new System.Drawing.Point(101, 25);
+            this.cmbEscolha.Name = "cmbEscolha";
+            this.cmbEscolha.Size = new System.Drawing.Size(160, 28);
+            this.cmbEscolha.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 362);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Backup and Restore";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cmbEscolha;
+        private System.Windows.Forms.MaskedTextBox mskPassword;
+        private System.Windows.Forms.MaskedTextBox mskServer;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDirectory;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox txtDataBase;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Backup/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+namespace Backup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            cmbEscolha.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String servidor = mskServer.Text.Replace(" ", "");
+            String usuario = txtUser.Text.Trim();
+            String senha = mskPassword.Text;
+            String banco = txtDataBase.Text;
+            String arquivo = txtDirectory.Text;
+
+            try
+            {
+                string constring = "server=" + servidor + ";user=" + usuario + ";pwd=" + senha + ";database=" + banco + ";";
+
+                constring += "charset=utf8;convertzerodatetime=true;";
+
+                using (MySqlConnection conn = new MySqlConnection(constring))
+                {
+                    conn.Close();
+                    using (MySqlCommand cmd = new MySqlCommand())
+                    {
+                        using (MySqlBackup mb = new MySqlBackup(cmd))
+                        {
+                            cmd.Connection = conn;
+                            conn.Open();
+
+                            if (cmbEscolha.SelectedItem == "Backup")
+                                mb.ExportToFile(arquivo);
+                            else
+                                mb.ImportFromFile(arquivo);
+                            conn.Close();
+                            MessageBox.Show("Sucess");
+                        }
+                    }
+                }
+            }
+            catch(Exception erro)
+            {
+                MessageBox.Show("Erro: \n" + erro);
+            }
+                
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (cmbEscolha.SelectedItem == "Backup")
+            {
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.Filter = "Sql|*.sql";
+                saveFileDialog1.Title = "Save backup in";
+
+                string dia = DateTime.Now.Day.ToString();
+                string mes = DateTime.Now.Month.ToString();
+                string ano = DateTime.Now.Year.ToString();
+                string hora = DateTime.Now.ToLongTimeString().Replace(":", "");
+                string nomeDoArquivo = ano + "-" + mes + "-" + dia + "_" + hora;
+                saveFileDialog1.InitialDirectory = @"C:\Backup\";
+
+                if (saveFileDialog1.CheckPathExists)
+                    Directory.CreateDirectory(saveFileDialog1.InitialDirectory);
+
+                saveFileDialog1.FileName = nomeDoArquivo;
+
+                if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    txtDirectory.Text = saveFileDialog1.FileName;
+            }
+            else
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+                openFileDialog1.Filter = "Sql|*.sql";
+                openFileDialog1.Title = "Select a Sql File";
+
+                if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    txtDirectory.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(mskServer.Text.Replace(" ",""));
+        }
+    }
+}
